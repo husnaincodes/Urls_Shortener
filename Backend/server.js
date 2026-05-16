694e8ffe3851bf35619155a853b93438c1d9d4a1
Husnain <Husnain.tayab@hotmail.com>
2026-05-16 19:16:28 +0500

    fix: add dotenv config and dynamic port

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,17 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const urlRoutes = require('./routes/urlRoutes');
 
 const app = express();
+
 connectDB();
 
 app.use(cors());
 app.use(express.json());
-
 app.use('/', urlRoutes);
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
