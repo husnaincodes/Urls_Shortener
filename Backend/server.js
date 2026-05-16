11013d729c30814207ace8fa93d75500a2943373
Husnain <Husnain.tayab@hotmail.com>
2026-05-16 21:51:23 +0500

    Backend

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const urlRoutes = require('./routes/urlRoutes');
+const { redirectUrl } = require('./controllers/urlController');
 
 const app = express();
 
@@ -11,7 +12,10 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+
 app.use('/api', urlRoutes);
+
+app.get('/:code', redirectUrl);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
